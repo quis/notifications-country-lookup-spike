f6e61f6e205073067bbfadfacedd1dbb438ce164
Chris Hill-Scott <me@quis.cc>
2020-03-10 17:53:37 +0000

    Make it a data checker

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
-from data import CountryDict, get_closest, get_postage, lookup, traceback
+import requests
+
+from flask import Flask, Markup, render_template, request
+from notifications_utils.countries import Country, CountryNotFoundError
 
 
 app = Flask(__name__)
@@ -8,55 +10,48 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     search_term = request.args.get('search_term', '')
-    result = ''
+    country = ''
     postage = ''
+    result = ''
 
     if search_term:
         try:
-            closest = get_closest(search_term)
-            postage = get_postage(closest)
-            postage = f'({postage} postage zone)'
-            result = f'✅ {closest}'
-        except IndexError:
-            result = f'⚠️ No country matches {search_term}'
-            postage = ''
+            country = Country(search_term).canonical_name
+            postage = Country(search_term).postage_zone
+            result = Markup("""
+                <p>✅</p>
+                <p>We’d address this letter to <b>{}</b></p>
+                <p>We’d charge for postage to <b>{}</b></p>
+            """.format(
+                country,
+                postage,
+            ))
+        except CountryNotFoundError:
+            result = Markup(
+                '<p>⚠️</p><p>We don’t think {} is a country</p>'.format(search_term)
+            )
 
     return render_template(
         'index.html',
         search_term=search_term,
         result=result,
+        country=country,
         postage=postage,
     )
 
 
-@app.route('/list')
-def country_list():
-    country_list = sorted([
-        str(canonical)
-        for canonical in set(lookup.values())
-    ])
-    synonyms = {
-        country: [
-            traceback[key] for key, value in lookup.items()
-            if value == country and key != CountryDict.make_key(country)
-        ]
-        for country in country_list
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    url = 'https://hookb.in/9XyoRoZZpPCLqyNYOaLp'
+
+    data = {
+        'search_term': request.form.get('search_term'),
+        'country': request.form.get('country'),
+        'postage': request.form.get('postage'),
+        'feedback': request.form.get('feedback'),
     }
+
+    r = requests.post(url, verify=False, json=data)
     return render_template(
-        'list.html',
-        country_list=synonyms,
+        'thanks.html'
     )
-
-
-@app.route('/list-raw')
-def list_raw():
-    country_list = sorted([
-        str(canonical)
-        for canonical in set(lookup.values())
-    ])
-    output = []
-    for country in country_list:
-        for key, value in sorted(lookup.items()):
-            if value == country and key != make_key(country):
-                output.append(f'({repr(key)}, {repr(country)}),')
-    return '\n'.join(sorted(output))
